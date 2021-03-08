f33c13e3b0e8aa79470f8d737ac1cc04f79bf3a2
harshitagarwal846 <harshitagarwal846@gmail.com>
2021-03-08 15:44:48 +0530

    MainPage

--- main.js
+++ main.js
@@ -1,45 +1,75 @@
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyCDMvFD5zthICujULAK7ocBmpZamJ7View",
-    authDomain: "gamebase-fa264.firebaseapp.com",
-    projectId: "gamebase-fa264",
-    storageBucket: "gamebase-fa264.appspot.com",
-    messagingSenderId: "307863343800",
-    appId: "1:307863343800:web:8a4bf144a06b04f4869a4d",
-    measurementId: "G-5L8T4Q6WGY"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCDMvFD5zthICujULAK7ocBmpZamJ7View",
+  authDomain: "gamebase-fa264.firebaseapp.com",
+  projectId: "gamebase-fa264",
+  storageBucket: "gamebase-fa264.appspot.com",
+  messagingSenderId: "307863343800",
+  appId: "1:307863343800:web:8a4bf144a06b04f4869a4d",
+  measurementId: "G-5L8T4Q6WGY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-      // User is signed in.
-      //   window.location.href="main.html";
-    } else {
-      window.location.href = "login.html";
-      // No user is signed in.
-    }
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    //   window.location.href="main.html";
+  } else {
+    window.location.href = "login.html";
+    // No user is signed in.
+  }
+});
+
+function logout() {
+  firebase.auth().signOut().catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
   });
+}
 
-  function logout() {
-    firebase.auth().signOut().catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
+let selected;
+function optSel() {
+  selected = document.getElementById("filterid").value;
 
-      window.alert("Error : " + errorMessage);
+  let game = firebase.database().ref().child(selected);
+
+  const details = (num) => {
+    game.child(num).on("value", (snap) => {
+      let image = snap.child("Image").val();
+      let loc = snap.child("Link").val();
+      document.getElementById("game" + (num)).style.backgroundImage = "url('" + image + "')";
+      document.getElementById("head" + (num)).innerHTML = snap.child("Name").val();
+      document.getElementById("desc" + (num)).innerHTML = snap.child("Desc").val();
+      document.getElementById("game" + (num)).addEventListener("click", () => {
+        // window.open(loc, "_blank");
+        location.href=loc;
+      });
+
+      let flip = document.getElementById("flip" + (num));
+      document.getElementById("game" + (num)).addEventListener("mouseover", () => {
+
+        flip.style.visibility = "visible";
+      });
+      document.getElementById("game" + (num)).addEventListener("mouseout", () => {
+
+        flip.style.visibility = "hidden";
+
+      });
     });
   }
 
+  for (let i = 1; i <= 8; i++) {
+    details(i);
+  }
+}
+function changed() {
+  optSel();
+}
 
-  let bestGame=firebase.database().ref().child("Best");
-  let bestGame1=bestGame.child("1");
-
-  bestGame1.on("value",(snap) =>{
-
-      let image1=snap.child("Image").val();
-      document.getElementById("game1").style.backgroundImage=url(image1);
 
 
-  });
