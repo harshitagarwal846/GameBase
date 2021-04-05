3e848970d53ef9f2515831edfdc4f0591a1965c7
harshitagarwal846 <harshitagarwal846@gmail.com>
2021-04-05 08:36:42 +0530

    federatedLogin

--- login.js
+++ login.js
@@ -48,7 +48,7 @@ slasheye.addEventListener('click', () => { //TogglePassword
 
 });
 
-function googlesign(){
+function googlesign(){    //Google Sign In
   var provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider).then((result) => {
     /** @type {firebase.auth.OAuthCredential} */
@@ -71,7 +71,36 @@ function googlesign(){
     window.alert(errorMessage);
   });
 }
-function reset() {
+
+function facebooksign(){    //Facebook Sign In
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider).then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // The signed-in user info.
+    var user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    // ...
+    window.alert(errorMessage);
+    console.log(errorCode);
+  });
+}
+function reset() {    //Forget Password
 
   var emailAddress = document.getElementById("email").value;
 
